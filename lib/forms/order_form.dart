e878d498959e6a07724bcd997b94d875c450081c
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2022-08-17 12:56:02 +0545

    Display product image after being chosen

--- lib/forms/order_form.dart
+++ lib/forms/order_form.dart
@@ -40,102 +40,106 @@ class _OrderFormState extends State<OrderForm> {
                 appBar: AppBar(
                   title: const Text('Enter order details'),
                 ),
-                body: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    // Choose a customer
-                    DropdownButton<Customer>(
-                      value: dropdownValue,
-                      icon: const Icon(Icons.person),
-                      onChanged: (value) {
-                        print('Chosen customer : $value');
-                        setState(() {
-                          dropdownValue = value;
-                        });
-                        customerId = value?.id ?? 0;
-                      },
-                      hint: const Text('Customer'),
-                      items: customers
-                          .map<DropdownMenuItem<Customer>>((Customer value) {
-                        return DropdownMenuItem<Customer>(
-                          value: value,
-                          child: Text(value.name),
-                        );
-                      }).toList(),
-                    ),
+                body: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      // Choose a customer
+                      DropdownButton<Customer>(
+                        value: dropdownValue,
+                        icon: const Icon(Icons.person),
+                        onChanged: (value) {
+                          print('Chosen customer : $value');
+                          setState(() {
+                            dropdownValue = value;
+                          });
+                          customerId = value?.id ?? 0;
+                        },
+                        hint: const Text('Customer'),
+                        items: customers
+                            .map<DropdownMenuItem<Customer>>((Customer value) {
+                          return DropdownMenuItem<Customer>(
+                            value: value,
+                            child: Text(value.name),
+                          );
+                        }).toList(),
+                      ),
 
-                    // Choose a product
-                    Visibility(
-                      visible: productChosen,
-                      replacement: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            productId = await Navigator.pushNamed(
-                                context, '/products',
-                                arguments: true) as int?;
-                            if (productId != null) {
-                              print("Product chose : $productId");
-                              setState(() {
-                                productChosen = true;
-                              });
-                            }
-                          },
-                          child: Container(
-                            child: Image(
-                              image: AssetImage(
-                                  ProductHelper.getProductResourceFromID(
-                                      productId ?? 101)['imgUrl'] as String),
-                            ),
+                      // Choose a product
+                      Visibility(
+                        visible: productId != null,
+                        replacement: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              var id = await Navigator.pushNamed(
+                                      context, '/products', arguments: true)
+                                  as int?;
+                              if (id != null) {
+                                print("Product chose : $id");
+                                productId = id;
+                                setState(() {
+                                  productChosen = true;
+                                });
+                              }
+                            },
+                            child: const Text('Choose Product'),
+                          ),
+                        ),
+                        child: Container(
+                          child: Image(
+                            image: AssetImage(
+                                ProductHelper.getProductResourceFromID(
+                                    productId ?? 101)['imgUrl'] as String),
                           ),
                         ),
                       ),
-                      child: Text('Product id : $productId'),
-                    ),
-                    //Date Picker
-                    Visibility(
-                      visible: deadlinePicked,
-                      replacement: Padding(
+                      //Date Picker
+                      Visibility(
+                        visible: deadlinePicked,
+                        replacement: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              var results = await showCalendarDatePicker2Dialog(
+                                  context: context,
+                                  config: config,
+                                  dialogSize: const Size(325, 400));
+                              if (results != null && results.isNotEmpty) {
+                                deadline = results[0];
+                                setState(() {
+                                  deadlinePicked = true;
+                                });
+                              }
+                            },
+                            child: const Text('Deadline'),
+                          ),
+                        ),
+                        child: Text(
+                            "Deadline: ${deadline.toString().replaceAll('00:00:00.000', '')}"),
+                      ),
+                      // Place the order
+                      Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: ElevatedButton(
-                          onPressed: () async {
-                            var results = await showCalendarDatePicker2Dialog(
-                                context: context,
-                                config: config,
-                                dialogSize: const Size(325, 400));
-                            if (results != null && results.isNotEmpty) {
-                              setState(() {
-                                deadlinePicked = true;
-                              });
+                          onPressed: () {
+                            if (deadline != null &&
+                                productId != null &&
+                                customerId != null) {
+                              final order = Order(
+                                  deadline: deadline as DateTime,
+                                  productId: productId as int,
+                                  customerId: customerId as int);
+                              database.insertOrder(order);
+
+                              Navigator.pop(context);
                             }
                           },
-                          child: const Text('Deadline'),
+                          child: const Text('Place'),
                         ),
-                      ),
-                      child: Text(
-                          "Deadline: ${deadline.toString().replaceAll('00:00:00.000', '')}"),
-                    ),
-                    // Place the order
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (deadline != null &&
-                              productId != null &&
-                              customerId != null) {
-                            final order = Order(
-                                deadline: deadline as DateTime,
-                                productId: productId as int,
-                                customerId: customerId as int);
-                            database.insertOrder(order);
-
-                            Navigator.pop(context);
-                          }
-                        },
-                        child: const Text('Place'),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               );
             }
